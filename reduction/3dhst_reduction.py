a8a462d1fd21225fe2bb52c00e6a8a4ada2fd309
Rongpu Zhou <rongpu.zhou@gmail.com>
2017-07-19 11:48:24 -0400

    remove negative or zero z_mag_grism objects from 3D-HST

---
+++ reduction/3dhst_reduction.py
@@ -0,0 +1,20 @@
+# Split 3D-HST master catalog into separate catlogs in each field; 
+# Remove objects with negative or zero z_max_grism
+
+from __future__ import division, print_function
+import numpy as np
+import sys
+from astropy.table import Table
+
+cat = Table.read('/Users/roz18/Documents/Data/desi-truth-table/3dhst.v4.1.5.master.fits.gz')
+print(len(cat))
+mask = cat['z_max_grism']>0
+cat = cat[mask]
+print(len(cat))
+
+fields = np.unique(cat['field'])
+
+for field in fields:
+    mask = cat['field']==field
+    cat1 = cat[mask].copy()
+    cat1.write('/Users/roz18/Documents/Data/desi-truth-table/3dhst.v4.1.5.master.'+field+'.reduced.fits')
