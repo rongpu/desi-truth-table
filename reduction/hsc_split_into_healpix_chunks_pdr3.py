f611c35caec1aaaebbf61b1bf6ee87b5fe3e45ca
Rongpu Zhou <rongpu.zhou@gmail.com>
2026-07-10 14:44:14 -0700

    .

--- reduction/hsc_split_into_healpix_chunks_pdr3.py
+++ reduction/hsc_split_into_healpix_chunks_pdr3.py
@@ -14,17 +14,17 @@ hsc_list = [
 
 for field in ['north', 'south']:
     
-    cat_dir = '/global/cfs/cdirs/desi/target/analysis/truth/dr9.0/{}/matched/'.format(field)
-    # cat_dir = '/global/cscratch1/sd/rongpu/truth/dr9.0/{}/matched/'.format(field)
+    cat_dir = '/global/cfs/cdirs/desi/target/analysis/truth/dr11.0/{}/matched/'.format(field)
+    # cat_dir = '/global/cscratch1/sd/rongpu/truth/dr11.0/{}/matched/'.format(field)
     
     for index in range(len(hsc_list)):
         hsc_fn = hsc_list[index]
 
-        if os.path.isfile(os.path.join(cat_dir, 'ls-dr9.0-'+hsc_fn+'-reduced-match.fits')):
+        if os.path.isfile(os.path.join(cat_dir, 'ls-dr11.0-'+hsc_fn+'-reduced-match.fits')):
 
             print(hsc_fn)
 
-            cat1 = fitsio.read(os.path.join(cat_dir, 'ls-dr9.0-'+hsc_fn+'-reduced-match.fits'))
+            cat1 = fitsio.read(os.path.join(cat_dir, 'ls-dr11.0-'+hsc_fn+'-reduced-match.fits'))
             cat2 = fitsio.read(os.path.join(cat_dir, hsc_fn+'-reduced-match.fits'))
 
             ra, dec = cat1['RA'], cat1['DEC']
@@ -39,7 +39,7 @@ for field in ['north', 'south']:
 
             for pix_i in pix_unique:
                 mask = pix==pix_i
-                fitsio.write(os.path.join(output_dir, 'ls-dr9.0-'+hsc_fn+'-reduced-match-hp-{}.fits'.format(pix_i)), cat1[mask])
+                fitsio.write(os.path.join(output_dir, 'ls-dr11.0-'+hsc_fn+'-reduced-match-hp-{}.fits'.format(pix_i)), cat1[mask])
                 fitsio.write(os.path.join(output_dir, hsc_fn+'-reduced-match-hp-{}.fits'.format(pix_i)), cat2[mask])
 
             gc.collect()
