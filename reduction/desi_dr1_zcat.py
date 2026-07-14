e35cf5de16c66361ec530bca2b523aa9ef9c280e
Rongpu Zhou <rongpu.zhou@gmail.com>
2026-07-14 11:39:40 -0700

    .

--- reduction/desi_dr1_zcat.py
+++ reduction/desi_dr1_zcat.py
@@ -3,6 +3,8 @@ import numpy as np
 from astropy.table import Table, vstack, hstack, join
 import fitsio
 
+#####################################################################################################################
+
 cat = Table(fitsio.read('/pscratch/sd/r/rongpu/tmp/iron/zcatalog/v2/zall/zall-tilecumulative-iron.fits'))
 print(len(cat), len(np.unique(cat['TARGETID'])))
 
@@ -11,3 +13,14 @@ cat = cat[mask]
 print(len(cat), len(np.unique(cat['TARGETID'])))
 
 cat.write('/global/cfs/cdirs/desi/target/analysis/truth/parent/zall-tilecumulative-iron-unique.fits')
+
+#####################################################################################################################
+
+cat = Table(fitsio.read('/pscratch/sd/r/rongpu/tmp/iron/zcatalog/v2/zall/zall-pix-iron.fits'))
+print(len(cat), len(np.unique(cat['TARGETID'])))
+
+mask = cat['ZCAT_PRIMARY'].copy()
+cat = cat[mask]
+print(len(cat), len(np.unique(cat['TARGETID'])))
+
+cat.write('/global/cfs/cdirs/desi/target/analysis/truth/parent/zall-pix-iron-unique.fits')
